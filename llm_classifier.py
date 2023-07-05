86d7349dd43bd39534dc914290f272c9b70fd39e
juanluis1702 <juanluis1702@gmail.com>
2023-07-05 21:12:31 +0200

    add test set and evaluation

--- llm_classifier.py
+++ llm_classifier.py
@@ -132,11 +132,16 @@ for classifier_name, classifier_dir in estimators:
         labels = [label2id[x] for x in test_data['label'].tolist()]
 
         predictions = []
+        scores = []
         for text in texts:
             prediction = classifier(text)
             predictions.append(label2id[prediction["label"]])
+            scores.append(prediction["score"])
 
-        res = classification_report(y_true=labels, y_pred=predictions, output_dict=True)
+        res = classification_report(y_true=labels, y_pred=predictions, output_dict=True,
+                                    target_names=["car", "home", "life", "health", "sports"])
         acc_ = accuracy_score(y_true=labels, y_pred=labels)
+        roc_au_score_macro = roc_auc_score(labels, scores, average='macro')
+        roc_au_score_micro = roc_auc_score(labels, scores, average='micro')
         print(res)
         print(acc_)
